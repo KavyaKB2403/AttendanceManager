e17f3052a2e8d547519e56ebbe55a43d4748b194
KavyaKB2403 <kavyabhalia123@gmail.com>
2025-10-04 21:13:13 +0530

    debugging api 2

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,7 +2,7 @@
 import axios, { AxiosHeaders } from "axios";
 
 // Read from env (CRA-style) with fallback to local FastAPI default
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE; //?? "http://localhost:8000";
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
 console.log("API Base URL:", API_BASE);
 
 export const api = axios.create({
