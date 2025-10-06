a4524710e2d1e7f090a0cbbeac3c56ec6c4891e4
KavyaKB2403 <kavyabhalia123@gmail.com>
2025-10-06 19:58:32 +0530

    checking database

--- backend/db.py
+++ backend/db.py
@@ -2,10 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL","postgresql://postgres:kavyapost%402403@localhost:5432/attendance_manager"
-)
-
+DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL:", DATABASE_URL)
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
